f148203ae2b9c6d3dd5aa13ac4325b95d3f9b3c4
dzuberi <dzuberi522@gmail.com>
2025-08-05 21:50:11 +0000

    working script for pwl gen

--- stimulus/vcd2pwl.py
+++ stimulus/vcd2pwl.py
@@ -30,13 +30,18 @@ token_table = {
 
 def parser(line):
     for key, pattern in token_table.items():
-        if re.match(pattern, line):
+        if re.match(pattern, line, re.IGNORECASE):
             return key
     return "NULL"
 
 def deletelastlinefromfile(file1, file2):
     os.system(f"sed '$d' {file1} > {file2}")
     os.system(f"mv {file2} {file1}")
+
+def sanitize_filename(name):
+    # Replace any character not allowed in Linux file names with _
+    # Allowed: letters, numbers, dot, dash, underscore
+    return re.sub(r'[^A-Za-z0-9._-]', '_', name)
 
 # Get environment variables
 home = os.environ.get("HOME", ".")
@@ -52,7 +57,10 @@ except IOError:
     sys.exit(1)
 
 vcd_file_name += ".vcd"
+os.makedirs("./vcd", exist_ok=True)
 os.chdir("./vcd")
+os.makedirs("pwls", exist_ok=True)
+os.makedirs("tmp_pwls", exist_ok=True)
 
 # Check for correct number of command line arguments
 if len(sys.argv) != 1:
@@ -80,8 +88,15 @@ while line:
     if token == "TIME":
         line = vcd_fid.readline()
         fields = line.split()
-        value = float(fields[0])
-        unit = fields[1]
+        # Support timescale like "1ps", "1ns", etc.
+        timescale = fields[0] + fields[1] if len(fields) > 1 else fields[0]
+        m = re.match(r"(\d+(?:\.\d+)?)([a-zA-Z]+)", timescale)
+        if m:
+            value = float(m.group(1))
+            unit = m.group(2)
+        else:
+            print(f"Invalid timescale format: {timescale}")
+            sys.exit(1)
         if unit == "ns":
             multiplier = 1e-9 * value
         elif unit == "ps":
@@ -103,7 +118,7 @@ while line:
         keys = symbol_table.keys()
         for key in keys:
             signal_name = symbol_table[key]
-            file_name = f"{signal_name}.pwl"
+            file_name = f"pwls/{sanitize_filename(signal_name)}.pwl"
             fid[key] = open(file_name, "w")
 
         line = vcd_fid.readline()
@@ -132,7 +147,7 @@ while line:
 
     elif token == "VCD":
         value = line[0]
-        symbol = line[1]
+        symbol = re.sub('\n','',line[1:])
         voltage = HI if value == '0' else LO
         line_out = f"{multiplier * time:g} {voltage:g}\n"
         file2 = "tmp_" + fid[symbol].name
@@ -169,7 +184,7 @@ while line:
 # Cleanup
 vcd_fid.close()
 for key in symbol_table.keys():
-    name = symbol_table[key] + ".pwl"
+    name = sanitize_filename(symbol_table[key]) + ".pwl"
     print(f"Successfully created: {name}")
     fid[key].close()
 print("")
